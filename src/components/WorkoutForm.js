bfc9ddee92f6709cd7360ba28a5c5713432c6eaf
shiqi <2014878@myrp.edu.sg>
2025-01-19 21:46:10 +0800

    h

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -15,13 +15,9 @@ const WorkoutForm = () => {
 
     const workout = {title, load, reps}
     
-    const response = await fetch('/api/workouts', {
-      method: 'POST',
-      body: JSON.stringify(workout),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/workouts`,
+    )
     const json = await response.json()
 
     if (!response.ok) {
